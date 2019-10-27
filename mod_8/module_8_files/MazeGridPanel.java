36c6c519e4aff711d4565c87afe2c3f7ad5911c7
Jordan Zehr <jordan.zehr@temple.edu>
2019-10-27 10:51:57 -0400

    maze homework

---
+++ mod_8/module_8_files/MazeGridPanel.java
@@ -0,0 +1,160 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+import javax.swing.JPanel;
+
+public class MazeGridPanel extends JPanel{
+	private int rows;
+	private int cols;
+	private Cell[][] maze;
+
+
+
+	// extra credit
+	public void genDFSMaze() {
+		boolean[][] visited;
+		Stack<Cell> stack  = new Stack<Cell>();
+		Cell start = maze[0][0];
+		stack.push(start);
+	}
+
+
+	// add function to check direction
+
+
+	//homework
+	public void solveMaze() {
+		Stack<Cell> stack  = new Stack<Cell>();
+		Cell start = maze[0][0];
+		start.setBackground(Color.GREEN);
+		Cell finish = maze[rows-1][cols-1];
+		finish.setBackground(Color.RED);
+		stack.push(start);
+
+		Cell current = stack.peek();
+		while(!(current.getBackground() == Color.RED) && !stack.isEmpty()) {
+			current = stack.peek();
+			if(current.getBackground() == Color.RED) {
+				System.out.println("BINGO, we found it");
+				break;
+			}else{
+				System.out.println("still searching");
+				current.setBackground(Color.GREEN);
+			}
+
+			// check for north wall
+			if(!current.northWall){
+				Cell newCell = maze[current.row - 1][current.col];
+				if (!visited(newCell.row, newCell.col)) {
+					stack.push(newCell);
+					continue;
+				}
+			}
+			if(!current.southWall){
+				Cell newCell = maze[current.row + 1][current.col];
+				if (!visited(newCell.row, newCell.col)) {
+					stack.push(newCell);
+					continue;
+				}
+			}
+			if(!current.eastWall){
+				Cell newCell = maze[current.row][current.col + 1];
+				if (!visited(newCell.row, newCell.col)) {
+					stack.push(newCell);
+					continue;
+				}
+			}
+			if(!current.westWall){
+				Cell newCell = maze[current.row][current.col - 1];
+				if (!visited(newCell.row, newCell.col)) {
+					stack.push(newCell);
+					continue;
+				}
+			}
+			current.setBackground(Color.GRAY);
+			Cell bad = stack.pop();
+		}
+	}
+
+
+	
+
+	
+
+
+	public boolean visited(int row, int col) {
+		Cell c = maze[row][col];
+		Color status = c.getBackground();
+		if(status.equals(Color.WHITE)  || status.equals(Color.RED)  ) {
+			return false;
+		}
+
+
+		return true;
+
+
+	}
+
+
+	public void genNWMaze() {
+		
+		for(int row = 0; row  < rows; row++) {
+			for(int col = 0; col < cols; col++) {
+
+				if(row == 0 && col ==0) {
+					continue;
+				}
+
+				else if(row ==0) {
+					maze[row][col].westWall = false;
+					maze[row][col-1].eastWall = false;
+				} else if(col == 0) {
+					maze[row][col].northWall = false;
+					maze[row-1][col].southWall = false;
+				}else {
+					boolean north = Math.random()  < 0.5;
+					if(north ) {
+						maze[row][col].northWall = false;
+						maze[row-1][col].southWall = false;
+					} else {  // remove west
+						maze[row][col].westWall = false;
+						maze[row][col-1].eastWall = false;
+					}
+					maze[row][col].repaint();
+				}
+			}
+		}
+		this.repaint();
+		
+	}
+
+	public MazeGridPanel(int rows, int cols) {
+		this.setPreferredSize(new Dimension(800,800));
+		this.rows = rows;
+		this.cols = cols;
+		this.setLayout(new GridLayout(rows,cols));
+		this.maze =  new Cell[rows][cols];
+		for(int row = 0 ; row  < rows ; row++) {
+			for(int col = 0; col < cols; col++) {
+
+				maze[row][col] = new Cell(row,col);
+
+				this.add(maze[row][col]);
+			}
+
+		}
+
+
+		this.genNWMaze();
+		this.solveMaze();
+		
+	}
+
+
+
+
+}
