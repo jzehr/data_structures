d206e0b162358a2aa0c29161c3a10e1ac06dd362
Jordan Zehr <jordan.zehr@temple.edu>
2019-12-09 10:37:12 -0500

    CheatersHangman

---
+++ CHM/src/help.java
@@ -0,0 +1,45 @@
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/*
+public class help {
+    private int guessesLeft;
+    public List
+
+    public static Map<Integer, List<String>> generateInitialWordLists() {
+        // open dictionary file
+        try{
+            Scanner scanner = new Scanner(new File(filename))
+            `
+        }
+        // read in all the words
+        // split them up into seperate lists based on their size
+        // return the map
+    }
+
+*/
+    /* guesses e t
+     * wordList [tail, soot, rook, torn, tips]
+     *           t___, ___t
+     *
+     * { t___: [tail,torn,tips]
+     *   ___t: [soot]
+     *   ____: [rook]
+     * }
+     *
+     *
+     * */
+
+/*
+    public static Map<String,List<String>> generateWordFamilies(Set guesses, List wordList) {
+        //create new map
+        System.out.println("bob");
+    }
+
+
+    public static List<String> chooseNewWordList(Map<String,List<String>> families){
+        System.out.println("bob");
+    }
+}
+*/
